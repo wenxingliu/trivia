566718f6a927725e41bb7034328e912339b61ec6
Wenxing Liu <wenxing.liu.09@gmail.com>
2020-12-25 11:59:05 -0700

    updated tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -33,6 +33,147 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_categories_success(self):
+        res = self.client().get('/categories')
+        self.assertEqual(res.status_code, 200)
+
+    def test_get_questions_success(self):
+        res = self.client().get('/questions')
+        self.assertEqual(res.status_code, 200)
+
+    def test_get_questions_paginated_success(self):
+        res = self.client().get('/questions?page=1')
+        self.assertEqual(res.status_code, 200)
+
+    def test_get_questions_no_resource_found(self):
+        res = self.client().get('/questions?page=100')
+        self.assertEqual(res.status_code, 404)
+
+    def test_delete_question_by_id_success(self):
+        res = self.client().delete('/questions/2')
+        self.assertEqual(res.status_code, 200)
+
+    def test_delete_question_by_id_id_not_exist(self):
+        res = self.client().delete('/questions/1')
+        self.assertEqual(res.status_code, 422)
+
+    def test_submit_question_success(self):
+        request = {
+            "question": "What is my name?",
+            "answer": "WLIU",
+            "category": 1,
+            "difficulty": 5
+        }
+        res = self.client().post('/questions', 
+                                 data=json.dumps(request),
+                                 content_type='application/json')
+        self.assertEqual(res.status_code, 200)
+
+    def test_submit_question_missing_field(self):
+        request = {
+            "question": "What is your name?",
+            "answer": "XXX",
+            "difficulty": 1
+        }
+        res = self.client().post('/questions', 
+                                 data=json.dumps(request),
+                                 content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_search_questions_success(self):
+        request = {
+            "searchTerm": "What"
+        }
+        res = self.client().post('/questions/search', 
+                                 data=json.dumps(request),
+                                 content_type='application/json')
+        self.assertEqual(res.status_code, 200)
+
+    def test_search_questions_no_results(self):
+        request = {
+            "searchTerm": "xxxxxxxxxxx"
+        }
+        res = self.client().post('/questions/search', 
+                                 data=json.dumps(request),
+                                 content_type='application/json')
+        self.assertEqual(res.status_code, 404)
+
+    def test_get_questions_by_category_success(self):
+        res = self.client().get('/categories/1/questions')
+        self.assertEqual(res.status_code, 200)
+
+    def test_get_questions_by_category_no_results(self):
+        res = self.client().get('/categories/100/questions')
+        self.assertEqual(res.status_code, 404)
+
+    def test_play_quizzes_all_success(self):
+        request = {
+            "previous_questions": [],
+            "quiz_category": {
+                "id": 0,
+                "type": "All"
+            }
+        }
+        res = self.client().post('/quizzes',
+                                 data=json.dumps(request),
+                                 content_type='application/json')
+        self.assertEqual(res.status_code, 200)
+
+    def test_play_quizzes_category_1_success(self):
+        request = {
+            "previous_questions": [],
+            "quiz_category": {
+                "id": 1,
+                "type": "Science"
+            }
+        }
+        res = self.client().post('/quizzes',
+                                 data=json.dumps(request),
+                                 content_type='application/json')
+        self.assertEqual(res.status_code, 200)
+
+    def test_play_quizzes_category_1_last_question_success(self):
+        request = {
+            "previous_questions": [20, 21],
+            "quiz_category": {
+                "id": 1,
+                "type": "Science"
+            }
+        }
+        res = self.client().post('/quizzes',
+                                 data=json.dumps(request),
+                                 content_type='application/json')
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(res.get_json()['question'] is not None)
+
+    def test_play_quizzes_category_1_no_question_left_success(self):
+        request = {
+            "previous_questions": [20, 21, 22],
+            "quiz_category": {
+                "id": 1,
+                "type": "Science"
+            }
+        }
+        res = self.client().post('/quizzes',
+                                 data=json.dumps(request),
+                                 content_type='application/json')
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(res.get_json()['question'] is None)
+
+    def test_play_quizzes_category_bad_request(self):
+        request = {
+            "quiz_category": {
+                "id": 1,
+                "type": "Science"
+            }
+        }
+        res = self.client().post('/quizzes',
+                                 data=json.dumps(request),
+                                 content_type='application/json')
+
+        self.assertEqual(res.status_code, 400)
 
 
 # Make the tests conveniently executable
